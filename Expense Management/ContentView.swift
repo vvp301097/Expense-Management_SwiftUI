2ea4b31134ca3d758d6c530033f7948ff0605fde
Phat Vuong <vvp301097@gmail.com>
2024-10-10 15:02:55 +0700

    Initial Commit

---
+++ Expense Management/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Expense Management
+//
+//  Created by Phat Vuong Vinh on 10/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Expense Management/Expense_ManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_ManagementApp.swift
+//  Expense Management
+//
+//  Created by Phat Vuong Vinh on 10/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Expense_ManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
